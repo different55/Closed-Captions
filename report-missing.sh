700c880d69e689e5586abb53c7bb727044ce42f0
Different55 <different55@protonmail.com>
2026-04-21 23:00:35 -0400

    Also report missing language entries.

--- report-missing.sh
+++ report-missing.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env fish
-argparse 'd/domain=' -- $argv
+argparse 'd/domain=' 'l/language=' -- $argv
 or return
 
+set -l language "en"
+set -ql _flag_language
+and set language $_flag_language
+
 set -l domain "survival"
-set -ql _flat_domain
-and set domain $_flat_domain
+set -ql _flag_domain
+and set domain $_flag_domain
 
 set -l asset_directory "/Applications/Vintage Story.app/assets/"$domain"/sounds/"
+set -l lang_file "ClosedCaptions/assets/captions/lang/"$language".json"
 set -l data_file "ClosedCaptions/assets/captions/captions/"$domain".json"
 
 stat $asset_directory >/dev/null
@@ -19,6 +24,9 @@ for key in $keys
 	# Reformat key to strip numbers.
 	set clean_key (string replace -r '[/\d_-]+$' "" $key)
 	if not jq -e '.["'$clean_key'"]' $data_file >/dev/null
-		echo $clean_key is missing.
+		echo $clean_key" has no metadata in "$domain".json"
+	end
+	if not jq -e '.["captions:'$clean_key'"]' $lang_file >/dev/null
+		echo $clean_key" has no language data in "$language".json"
 	end
 end | sort | uniq
