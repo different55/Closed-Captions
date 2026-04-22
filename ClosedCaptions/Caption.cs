df6e4ecb8ac3f1df001ba6cd97538b31e0a79690
Different55 <different55@protonmail.com>
2026-04-22 00:03:56 -0400

    Re-import migrated EnumSoundType.
    At least I think it's migrated. It was fine before 1.22.

--- ClosedCaptions/Caption.cs
+++ ClosedCaptions/Caption.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using Vintagestory.API.Common;
 using Vintagestory.API.Client;
 using Vintagestory.API.Config;
 using Vintagestory.API.MathTools;
